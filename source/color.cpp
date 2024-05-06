768c8b133ab24100dad92923a1234ef0fc48eddf
Orange <orange-cpp@yandex.ru>
2024-05-07 02:12:16 +0300

    added constructors

--- source/color.cpp
+++ source/color.cpp
@@ -14,6 +14,16 @@ namespace uml::color
     
     Color Color::Blend(const Color &other, float ratio) const
     {
+        return *this * (1.f - std::clamp(ratio, 0.f, 1.f)) + other * ratio;
+    }
+
+    Color::Color(float r, float g, float, float b, float a) : Vector4(r,g,b,a)
+    {
+
+    }
+
+    Color::Color(const Vector4 &vec) : Vector4(vec.x, vec.y, vec.z, vec.w)
+    {
 
     }
 }
