8fc9a5495877daedbc1ac87ce53db657f89fc88c
Orange <orange-cpp@yandex.ru>
2024-08-27 13:12:35 +0300

    patched up matrixes

--- tests/UnitTestProjection.cpp
+++ tests/UnitTestProjection.cpp
@@ -8,8 +8,8 @@
 
 TEST(UnitTestProjection, IsPointOnScreen)
 {
-    const omath::projection::Camera camera({}, {90, 0.f, 0.f} , {1920.f, 1080.f, 0.f}, 120, 10, 100);
+        const omath::projection::Camera camera({}, {90, 0.f, 0.f} , {1920.f, 1080.f, 0.f}, 120, 10, 100);
 
-    const auto proj = camera.WorldToScreen({0,0,15});
+    const auto proj = camera.WorldToScreen({0, 0, 20});
     EXPECT_TRUE(proj.has_value());
 }
