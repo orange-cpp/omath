e5dbb78b1586e8ebb53bebc0a819549b0a891e81
Orange <orange-cpp@yandex.ru>
2024-08-27 17:35:17 +0300

    patch

--- tests/UnitTestProjection.cpp
+++ tests/UnitTestProjection.cpp
@@ -1,6 +1,7 @@
 //
 // Created by Vlad on 27.08.2024.
 //
+#include <complex>
 #include <gtest/gtest.h>
 #include <omath/Matrix.h>
 #include <print>
@@ -8,8 +9,10 @@
 
 TEST(UnitTestProjection, IsPointOnScreen)
 {
-        const omath::projection::Camera camera({}, {90, 0.f, 0.f} , {1920.f, 1080.f, 0.f}, 120, 10, 100);
+        const omath::projection::Camera camera({0, 0, 0}, {0, 0.f, 0.f} , {1920.f, 1080.f, 0.f}, 110, 0.1, 500);
 
-    const auto proj = camera.WorldToScreen({0, 0, 20});
+    const auto proj = camera.WorldToScreen({5, 0, 0});
+    if (proj)
+        std::print("{} {} {}", proj->x, proj->z, proj->y);
     EXPECT_TRUE(proj.has_value());
 }
