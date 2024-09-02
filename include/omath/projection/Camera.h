311d38be02b81549c21944e4af38c98df5e741f1
Orange <orange-cpp@yandex.ru>
2024-09-03 02:04:35 +0300

    removed trash

--- include/omath/projection/Camera.h
+++ include/omath/projection/Camera.h
@@ -27,11 +27,7 @@ namespace omath::projection
         Camera(const Vector3& position, const Vector3& viewAngles, const ViewPort& viewPort, float fov, float near, float far);
         void SetViewAngles(const Vector3& viewAngles);
 
-        [[nodiscard]] const Vector3& GetViewAngles() const;
         [[nodiscard]] Matrix GetViewMatrix() const;
-        [[nodiscard]] Matrix GetProjectionMatrix() const;
-        [[nodiscard]] Matrix GetTranslationMatrix() const;
-        [[nodiscard]] Matrix GetOrientationMatrix() const;
 
         [[nodiscard]] std::expected<Vector3, std::string_view> WorldToScreen(const Vector3& worldPosition) const;
 
