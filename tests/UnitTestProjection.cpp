891f26b452c89b88378d9c7ff15754c60ab5cb36
Orange <orange-cpp@yandex.ru>
2024-10-14 11:32:41 +0300

    added more unit tests

--- tests/UnitTestProjection.cpp
+++ tests/UnitTestProjection.cpp
@@ -7,10 +7,17 @@
 #include <print>
 #include <omath/projection/Camera.h>
 
-TEST(UnitTestProjection, IsPointOnScreen)
+TEST(UnitTestProjection, Projection)
 {
-    const omath::projection::Camera camera({0.f, 0.f, 0.f}, {0, 0.f, 0.f} , {1920.f, 1080.f}, 110.f, 0.1f, 500.f);
+    const omath::projection::Camera camera({0.f, 0.f, 0.f}, {0, 0.f, 0.f} , {1920.f, 1080.f}, 110.f, 0.375f, 5000.f);
 
-    const auto proj = camera.WorldToScreen({100, 0, 15});
-    EXPECT_TRUE(proj.has_value());
+    EXPECT_EQ(camera.WorldToScreen({100, 0, 0}).value(), omath::Vector2(960, 540));
+    EXPECT_EQ(camera.WorldToScreen({49.23, 0, 0}).value(), omath::Vector2(960, 540));
+    const auto proj = camera.WorldToScreen({100, 50, -69});
+    std::print("{} {}", proj->x, proj->y);
+    EXPECT_EQ(camera.WorldToScreen({100, 10, 0}).value(), omath::Vector2(909.58887, 540));
+    EXPECT_EQ(camera.WorldToScreen({100, 10, 8}).value(), omath::Vector2(909.58887, 499.67108));
+    EXPECT_EQ(camera.WorldToScreen({100, 50, -69}).value(), omath::Vector2(707.9442, 887.83704));
+
+    EXPECT_FALSE(camera.WorldToScreen({-10,0, 0}).has_value());
 }
