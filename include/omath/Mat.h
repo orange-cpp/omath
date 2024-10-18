3e6cabb6c7db18328dd18378b88a09ed52b8aa30
Orange <orange-cpp@yandex.ru>
2024-10-18 17:20:23 +0300

    fixed projection matrix

--- include/omath/Mat.h
+++ include/omath/Mat.h
@@ -280,16 +280,18 @@ namespace omath
         }
 
         [[nodiscard]]
-        constexpr static Mat<4, 4> ProjectionMat(const float fieldOfView, const float aspectRatio, const float near, const float far)
+        constexpr static Mat<4, 4> ProjectionMat(const float fieldOfView, const float aspectRatio,
+                                                 const float near, const float far, const float lensZoom)
         {
             const float fovHalfTan = std::tan(angles::DegreesToRadians(fieldOfView) / 2.f);
+            const float frustumHeight = far - near;
 
             return
             {
-                {1.f / (aspectRatio * fovHalfTan), 0.f, 0.f, 0.f},
-                {0.f, 1.f / fovHalfTan, 0.f, 0.f},
-                {0.f, 0.f, (far + near) / (far - near), 2.f * near * far / (far - near)},
-                {0.f, 0.f, -1.f, 0.f}
+                {-1.f / (aspectRatio * fovHalfTan) * lensZoom, 0.f, 0.f, 0.f},
+                {0.f, -1.f / fovHalfTan * lensZoom, 0.f, 0.f},
+                {0.f, 0.f, -far / frustumHeight, -1},
+                {0.f, 0.f, near * far / frustumHeight, 0.f}
             };
         }
 
