6e5f2331adbe05054a7a01ddacfecb5ac9d8cb89
Orange <orange-cpp@yandex.ru>
2024-11-27 15:21:07 +0300

    added files

---
+++ include/omath/engines/source.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Orange on 11/24/2024.
+//
+#pragma once
+
+
+namespace omath::source
+{
+    constexpr Vector3 kAbsUp = {0, 0, 1};
+    constexpr Vector3 kAbsRight = {0, -1, 0};
+    constexpr Vector3 kAbsForward = {1, 0, 0};
+
+
+    template<class Type> requires std::is_floating_point_v<Type> || std::is_integral_v<Type>
+    [[nodiscard]] Mat<4, 4, Type, MatStoreType::COLUMN_MAJOR> PerspectiveProjectionMatrix(
+        const float fieldOfView, const Type &aspectRatio, const Type &near, const Type &far)
+    {
+        const float fovHalfTan = std::tan(angles::DegreesToRadians(fieldOfView) / 2);
+
+        return
+        {
+            {static_cast<Type>(1) / (aspectRatio * fovHalfTan), 0, 0, 0},
+            {0, static_cast<Type>(1) / (fovHalfTan), 0, 0},
+            {0, 0, (far + near) / (far - near), -(static_cast<Type>(2) * far * near) / (far - near)},
+            {0, 0, 1, 0},
+        };
+    }
+}
