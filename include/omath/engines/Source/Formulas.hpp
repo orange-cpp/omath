9789bc6c645b121d9e987b9af8adefc0061cc15a
Orange <orange-cpp@yandex.ru>
2024-12-16 12:31:56 +0300

    removed useless stuff

--- include/omath/engines/Source/Formulas.hpp
+++ include/omath/engines/Source/Formulas.hpp
@@ -4,7 +4,6 @@
 #pragma once
 #include "Constants.h"
 
-
 namespace omath::source
 {
     [[nodiscard]]
--- source/prediction/Projectile.cpp
+++ source/prediction/Projectile.cpp
@@ -10,9 +10,12 @@ namespace omath::prediction
 {
     Vector3 Projectile::PredictPosition(const float pitch, const float yaw, const float time, const float gravity) const
     {
-        auto currentPos = m_origin + omath::source::ForwardVector({source::PitchAngle::FromDegrees(-pitch), source::YawAngle::FromDegrees(yaw), source::RollAngle::FromDegrees(0)}) * m_launchSpeed * time;
-        currentPos.z -= (gravity * m_gravityScale) * std::pow(time, 2.f) * 0.5f;
+        auto currentPos = m_origin + source::ForwardVector({source::PitchAngle::FromDegrees(-pitch),
+                                                            source::YawAngle::FromDegrees(yaw),
+                                                            source::RollAngle::FromDegrees(0)}) *
+                                             m_launchSpeed * time;
+        currentPos.z -= (gravity * m_gravityScale) * (time * time) * 0.5f;
 
         return currentPos;
     }
-}
+} // namespace omath::prediction
