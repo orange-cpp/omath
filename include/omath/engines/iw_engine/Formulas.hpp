1b2822275f3b6ca058471ea6f54aed81b0fc2699
Orange <orange-cpp@yandex.ru>
2025-03-19 20:07:44 +0300

    removed inline functions

--- include/omath/engines/iw_engine/Formulas.hpp
+++ include/omath/engines/iw_engine/Formulas.hpp
@@ -8,49 +8,17 @@
 namespace omath::iw_engine
 {
     [[nodiscard]]
-    inline Vector3<float> ForwardVector(const ViewAngles& angles)
-    {
-        const auto vec = MatRotation(angles) * MatColumnFromVector(kAbsForward);
-
-        return {vec.At(0, 0), vec.At(1, 0), vec.At(2, 0)};
-    }
+    Vector3<float> ForwardVector(const ViewAngles& angles);
 
     [[nodiscard]]
-    inline Vector3<float> RightVector(const ViewAngles& angles)
-    {
-        const auto vec = MatRotation(angles) * MatColumnFromVector(kAbsRight);
-
-        return {vec.At(0, 0), vec.At(1, 0), vec.At(2, 0)};
-    }
+    Vector3<float> RightVector(const ViewAngles& angles);
 
     [[nodiscard]]
-    inline Vector3<float> UpVector(const ViewAngles& angles)
-    {
-        const auto vec = MatRotation(angles) * MatColumnFromVector(kAbsUp);
+    Vector3<float> UpVector(const ViewAngles& angles);
 
-        return {vec.At(0, 0), vec.At(1, 0), vec.At(2, 0)};
-    }
-
-    [[nodiscard]] inline Mat4x4 CalcViewMatrix(const ViewAngles& angles, const Vector3<float>& cam_origin)
-    {
-        return MatCameraView(ForwardVector(angles), RightVector(angles), UpVector(angles), cam_origin);
-    }
+    [[nodiscard]] Mat4x4 CalcViewMatrix(const ViewAngles& angles, const Vector3<float>& cam_origin);
 
 
     [[nodiscard]]
-    inline Mat4x4 CalcPerspectiveProjectionMatrix(const float fieldOfView, const float aspectRatio, const float near,
-                                                  const float far)
-    {
-        // NOTE: Need magic number to fix fov calculation, since source inherit Quake proj matrix calculation
-        constexpr auto kMultiplyFactor = 0.75f;
-        const float fovHalfTan = std::tan(angles::DegreesToRadians(fieldOfView) / 2.f) * kMultiplyFactor;
-
-        return
-        {
-            {1.f / (aspectRatio * fovHalfTan), 0, 0, 0},
-            {0, 1.f / fovHalfTan, 0, 0},
-            {0, 0, (far + near) / (far - near), -(2.f * far * near) / (far - near)},
-            {0, 0, 1, 0},
-        };
-    }
+    Mat4x4 CalcPerspectiveProjectionMatrix(float fieldOfView, float aspectRatio, float near, float far);
 } // namespace omath::source
