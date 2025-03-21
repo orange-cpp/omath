9383fd37fcec903aa086ac5052057750440383dd
Orange <orange-cpp@yandex.ru>
2025-03-21 04:47:01 +0300

    fix

--- include/omath/projectile_prediction/proj_pred_engine_avx2.hpp
+++ include/omath/projectile_prediction/proj_pred_engine_avx2.hpp
@@ -2,7 +2,7 @@
 // Created by Vlad on 2/23/2025.
 //
 #pragma once
-#include "proj_pred_engine.hpp"
+#include "omath/projectile_prediction/proj_pred_engine.hpp"
 
 namespace omath::projectile_prediction
 {
