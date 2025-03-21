5c8f730ac755eca597412cc2b704fd60ef85d65f
Orange <orange-cpp@yandex.ru>
2025-03-21 04:48:38 +0300

    style fix

--- include/omath/pathfinding/navigation_mesh.hpp
+++ include/omath/pathfinding/navigation_mesh.hpp
@@ -20,19 +20,19 @@ namespace omath::pathfinding
     class NavigationMesh final
     {
     public:
-
         [[nodiscard]]
         std::expected<Vector3<float>, std::string> GetClosestVertex(const Vector3<float>& point) const;
-
 
         [[nodiscard]]
         const std::vector<Vector3<float>>& GetNeighbors(const Vector3<float>& vertex) const;
 
         [[nodiscard]]
         bool Empty() const;
+
         [[nodiscard]] std::vector<uint8_t> Serialize() const;
+
         void Deserialize(const std::vector<uint8_t>& raw);
 
         std::unordered_map<Vector3<float>, std::vector<Vector3<float>>> m_verTextMap;
     };
-}
+} // namespace omath::pathfinding
