80e3cadb67ebd8f8b11242d49d183d65ccee5ae2
Orange <orange-cpp@yandex.ru>
2025-04-18 01:33:47 +0300

    added ratio param

--- include/omath/3d_primitives/box.hpp
+++ include/omath/3d_primitives/box.hpp
@@ -10,5 +10,5 @@ namespace omath::primitives
 {
     [[nodiscard]]
     std::array<Vector3<float>, 8> CreateBox(const Vector3<float>& top, const Vector3<float>& bottom,
-                                            const Vector3<float>& dirForward, const Vector3<float>& dirRight);
+                                            const Vector3<float>& dirForward, const Vector3<float>& dirRight, float ratio = 4.f);
 }
