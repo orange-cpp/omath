ce08fcdd29b99efd5639318a99c21f808325390e
Orange <orange-cpp@yandex.ru>
2025-05-04 19:03:18 +0300

    removed useless source files

--- source/projectile_prediction/proj_pred_engine.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Vlad on 2/23/2025.
-//
-#include "omath/projectile_prediction/proj_pred_engine.hpp"
-
-namespace omath::projectile_prediction
-{
-
-} // namespace omath::projectile_prediction
--- source/projection/camera.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Vlad on 27.08.2024.
-//
-#include "omath/projection/camera.hpp"
-
-namespace omath::projection
-{
-}
