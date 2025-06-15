2c710555d6ebbb5eb33537a1263487a1beb2c120
Orange <orange-cpp@yandex.ru>
2025-06-15 22:04:58 +0300

    fixed example

--- examples/example_proj_mat_builder.cpp
+++ examples/example_proj_mat_builder.cpp
@@ -15,8 +15,8 @@ int main()
     float fov = 0;
     float near = 0;
     float far = 0;
-    float viewPortWidth = 0;
-    float viewPortHeight = 0;
+    float view_port_width = 0;
+    float view_port_height = 0;
 
     std::print("Enter camera fov: ");
     std::cin >> fov;
@@ -28,13 +28,13 @@ int main()
     std::cin >> far;
 
     std::print("Enter camera screen width: ");
-    std::cin >> viewPortWidth;
+    std::cin >> view_port_width;
 
     std::print("Enter camera screen height: ");
-    std::cin >> viewPortHeight;
+    std::cin >> view_port_height;
 
     const auto mat =
-            omath::opengl_engine::CalcPerspectiveProjectionMatrix(fov, viewPortWidth / viewPortHeight, near, far);
+            omath::opengl_engine::calc_perspective_projection_matrix(fov, view_port_width / view_port_height, near, far);
 
-    std::print("{}", mat.ToString());
+    std::print("{}", mat.to_string());
 };
