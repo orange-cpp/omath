a6fc92dd3afabece1f1314912b9ef235da060ac3
Orange <orange-cpp@yandex.ru>
2025-08-03 18:35:52 +0300

    Updates project version and removes legacy code
    Updates the project version to prepare for a new release.
    
    Removes the legacy projectile prediction engine, which is no longer needed.

--- source/projectile_prediction/proj_pred_engine_legacy.cpp
+++
@@ -1,8 +0,0 @@
-#include "omath/projectile_prediction/proj_pred_engine_legacy.hpp"
-#include <cmath>
-#include <omath/angles.hpp>
-
-namespace omath::projectile_prediction
-{
-
-} // namespace omath::projectile_prediction
