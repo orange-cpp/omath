f1fbea21a7f3ef1d27cc2050a09fa8abebc040b3
Orange <orange-cpp@yandex.ru>
2025-08-03 17:33:22 +0300

    Refactors projectile prediction engine
    Refactors the projectile prediction engine by introducing an interface
    and making the legacy implementation more flexible.
    
    The legacy engine is updated to allow for coordinate system customization
    through virtual methods, enabling usage in different game environments.
    
    Also introduces vcpkg support for easier dependency management and adds boost-asio as a dependency.

--- include/omath/projectile_prediction/projectile.hpp
+++ include/omath/projectile_prediction/projectile.hpp
@@ -10,9 +10,6 @@ namespace omath::projectile_prediction
     class Projectile final
     {
     public:
-        [[nodiscard]]
-        Vector3<float> predict_position(float pitch, float yaw, float time, float gravity) const noexcept;
-
         Vector3<float> m_origin;
         float m_launch_speed{};
         float m_gravity_scale{};
