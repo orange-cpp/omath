8004c8c181c891c5fd03b40a998000bf035300c8
Orange <orange-cpp@yandex.ru>
2025-08-25 21:30:27 +0300

    Adds Unreal Engine math implementations
    Adds Unreal Engine-specific implementations for camera and projectile prediction calculations.
    
    This includes:
    - Defining constants and data types tailored for Unreal Engine's coordinate system and conventions.
    - Implementing functions for calculating forward, right, and up vectors, view matrices, and perspective projection matrices.
    - Providing camera trait for look-at angle calculations and projection matrix generation.
    - Implements projectile prediction traits and utilities.

---
+++ include/omath/engines/unreal_engine/traits/camera_trait.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Vlad on 8/10/2025.
+//
+
+#pragma once
+#include "omath/engines/unreal_engine/formulas.hpp"
+#include "omath/projection/camera.hpp"
+
+namespace omath::unreal_engine
+{
+    class CameraTrait final
+    {
+    public:
+        [[nodiscard]]
+        static ViewAngles calc_look_at_angle(const Vector3<float>& cam_origin, const Vector3<float>& look_at) noexcept;
+
+        [[nodiscard]]
+        static Mat4X4 calc_view_matrix(const ViewAngles& angles, const Vector3<float>& cam_origin) noexcept;
+        [[nodiscard]]
+        static Mat4X4 calc_projection_matrix(const projection::FieldOfView& fov, const projection::ViewPort& view_port,
+                                             float near, float far) noexcept;
+    };
+
+} // namespace omath::unity_engine
