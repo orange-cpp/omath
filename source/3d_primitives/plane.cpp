80938cd913d0e3da6e635a91803eb7fb84beab5c
Orange <orange-cpp@yandex.ru>
2025-08-28 23:18:36 +0300

    Adds plane primitive generation
    Implements a function to generate a plane primitive from two vertices, a direction vector, and a size, returning an array of two triangles.

---
+++ source/3d_primitives/plane.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Vlad on 8/28/2025.
+//
+#include "omath/3d_primitives/plane.hpp"
+
+namespace omath::primitives
+{
+    std::array<Triangle<Vector3<float>>, 2> create_plane(const Vector3<float>& vertex_a,
+                                                          const Vector3<float>& vertex_b,
+                                                          const Vector3<float>& direction, const float size) noexcept
+    {
+        Triangle<Vector3<float>> triangles;
+        const auto second_vertex_a = vertex_a + direction * size;
+        const auto second_vertex_b = vertex_b + direction * size;
+        return std::array
+        {
+            Triangle{second_vertex_a, vertex_a, vertex_b},
+            Triangle{second_vertex_b, vertex_b, vertex_a}
+        };
+    }
+} // namespace omath::primitives
