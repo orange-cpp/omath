c0353cd9be7e7d10a060a586b50dd11313ee9072
Orange <orange-cpp@yandex.ru>
2025-11-09 15:38:38 +0300

    updated test

--- tests/general/unit_test_gjk.cpp
+++ tests/general/unit_test_gjk.cpp
@@ -4,7 +4,7 @@
 #include <gtest/gtest.h>
 #include <omath/collision/gjk_algorithm.hpp>
 
-TEST(UnitTestGjk, TestCollision)
+TEST(UnitTestGjk, TestCollisionTrue)
 {
     const std::vector<omath::Vector3<float>> mesh = {
         {-1.f, -1.f, -1.f},
@@ -18,7 +18,29 @@ TEST(UnitTestGjk, TestCollision)
     };
 
     const omath::collision::MeshCollider collider_a(mesh, {0.f, 0.f, 0.f});
-    const omath::collision::MeshCollider collider_b(mesh, {0.f, 3.f, 0.f});
+    const omath::collision::MeshCollider collider_b(mesh, {0.f, 0.5f, 0.f});
 
-    const auto result = omath::collision::GjkAlgorithm::check_collision(collider_a, collider_b);
+    const auto result = omath::collision::GjkAlgorithm::is_collide(collider_a, collider_b);
+
+    EXPECT_TRUE(result);
+}
+TEST(UnitTestGjk, TestCollisionFalse)
+{
+    const std::vector<omath::Vector3<float>> mesh = {
+        {-1.f, -1.f, -1.f},
+        {-1.f, -1.f,  1.f},
+        {-1.f,  1.f, -1.f},
+        {-1.f,  1.f,  1.f},
+        { 1.f,  1.f,  1.f}, // x = +1 vertices (put {1,1,1} first in case your support breaks ties by first-hit)
+        { 1.f,  1.f, -1.f},
+        { 1.f, -1.f,  1.f},
+        { 1.f, -1.f, -1.f}
+    };
+
+    const omath::collision::MeshCollider collider_a(mesh, {0.f, 0.f, 0.f});
+    const omath::collision::MeshCollider collider_b(mesh, {0.f, 4.1f, 0.f});
+
+    const auto result = omath::collision::GjkAlgorithm::is_collide(collider_a, collider_b);
+
+    EXPECT_FALSE(result);
 }
