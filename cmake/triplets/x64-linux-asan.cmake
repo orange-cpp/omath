000c1f8e2d80f280f9ffd76beae3ea9696ca7b2f
Saikari <lin@sz.cn.eu.org>
2025-12-30 22:22:42 +0300

    Try use overlay triplet for sanitizer

---
+++ cmake/triplets/x64-linux-asan.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+set(VCPKG_CXX_FLAGS "-stdlib=libc++ -fsanitize=address,leak -fno-omit-frame-pointer -g -O2 -DNDEBUG")
+set(VCPKG_C_FLAGS "-fsanitize=address,leak -fno-omit-frame-pointer -g -O2 -DNDEBUG")
+set(VCPKG_LINKER_FLAGS "-stdlib=libc++ -fsanitize=address,leak")
