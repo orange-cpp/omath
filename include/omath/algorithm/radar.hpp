1982185b710e3b739c3048187b507520f836b22d
Orange <orange-cpp@yandex.ru>
2026-07-02 10:50:28 +0300

    code style fix

--- include/omath/algorithm/radar.hpp
+++ include/omath/algorithm/radar.hpp
@@ -12,15 +12,14 @@ namespace omath::algorithm
     [[nodiscard]]
     Vector2<float> world_to_radar(const Camera& camera, const Vector3<FloatingType>& position, const FloatingType scale)
     {
-
         const auto look_at_angles = camera.calc_look_at_angles(position);
         const auto current_angles = camera.get_view_angles();
 
         static const auto sign = [&camera, &current_angles]
         {
             auto right_yaw = current_angles.yaw
-                           - camera.calc_look_at_angles(camera.get_origin() + camera.get_abs_right()).yaw
-                           - decltype(current_angles.yaw)::from_degrees(90);
+                             - camera.calc_look_at_angles(camera.get_origin() + camera.get_abs_right()).yaw
+                             - decltype(current_angles.yaw)::from_degrees(90);
             return right_yaw.cos() < 0 ? -1.f : 1.f;
         }();
 
